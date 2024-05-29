6c3c9ee21e7a02ef53fc9f638da12465f7dfb03c
ZdmitryZ <o498650@gmail.com>
2024-05-30 01:05:54 +0500

    Imitial commit

---
+++ main.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WhtTime</title>
+</head>
+<style>
+    body{
+        color:aliceblue;
+        background: url(/grnd.jpg);
+        background-size: cover;
+    }
+</style>
+<body>
+    <h1 align="center">Вы не знаете что сейчас за окном? Утро, день, вечер, может быть полдень?</h1>
+    <h2 align="center">Впишите в поле ниже который час (1...24), и узнайте!</h2>
+    <h3 align="center">Если вы введете значение > 24, то программа посчитает, что это 24 часа.</h3>
+
+    <form action="" method="post">
+        <p align="center"> Час на улице: <input data-min="0" data-max="24" type="text" name="hour" class="hour" placeholder="Введите который час " onkeyup="this.value = this.value.replace(/[^\d]/g,'',);"> . </p>
+        <div style="text-align:center">
+            <button type="submit" name="submit" value="submit">Получить ответ!</button>
+        </div>
+        <h1 align="center">
+
+            <?php $day = 10;
+            if (isset($_POST['submit'])) {
+                $dat = $_POST['hour'];
+
+                if ($dat > 24) {
+                    $dat = 24;
+                }
+
+                if (($dat >= 5) & ($dat < 12)) {
+                    echo "$dat ч. - это утро!";
+                } elseif ($dat == 12) {
+                    echo "$dat ч. - это полдень!";
+                }
+                if (($dat >= 0) & ($dat < 5) || ($dat == 24)) {
+                    echo "$dat ч. - это ночь!";
+                }
+                if (($dat > 12) & ($dat < 18)) {
+                    echo "$dat ч. - это день!";
+                }
+                if (($dat >= 18) & ($dat < 24)) {
+                    echo "$dat ч. - это вечер!";
+                }
+            } ?></h1>
+
+    </form>
+
+</body>
+
+</html>
